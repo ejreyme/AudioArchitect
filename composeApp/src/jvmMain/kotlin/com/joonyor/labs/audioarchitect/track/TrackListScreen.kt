e16c329b422f28b41f7a0cced3e09dde6e0c559f
ejreyme <reyme3@gmail.com>
2025-08-09 22:52:54 -0400

    Refactor `TrackListScreen` with context menu and selection support; modularize track row UI.

--- composeApp/src/jvmMain/kotlin/com/joonyor/labs/audioarchitect/track/TrackListScreen.kt
+++ composeApp/src/jvmMain/kotlin/com/joonyor/labs/audioarchitect/track/TrackListScreen.kt
@@ -1,10 +1,14 @@
 package com.joonyor.labs.audioarchitect.track
 
+import androidx.compose.foundation.ContextMenuDataProvider
+import androidx.compose.foundation.ContextMenuItem
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material3.Text
@@ -31,54 +35,100 @@ fun TrackListScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         LazyColumn {
-            items(trackCollection.size) { track ->
-                Row(
-                    modifier = Modifier.fillParentMaxWidth()
-                        .combinedClickable(
-                            onClick = {
-                                selectedTrack.value = trackCollection[track]
-                            },
-//                            onDoubleClick = {
-//                                selectedTrack.value = trackCollection[track]
-//                                onMediaPlayerEvent.invoke(
-//                                    AudioPlayerEvent(
-//                                        track = selectedTrack.value,
-//                                        type = if (isPlaying) AudioPlayerEventType.PAUSE else AudioPlayerEventType.PLAY
-//                                    )
-//                                )
-//                            }
-                        )
+            items(trackCollection.size) { trackIndex ->
+                ContextMenuDataProvider(
+                    items = {
+                        generateContextMenus()
+                    }
                 ) {
-                    Column(modifier = Modifier.weight(0.2f)) {
-                        Button(
-                            onClick = {
-                                selectedTrack.value = trackCollection[track]
-                                onMediaPlayerEvent.invoke(
-                                    AudioPlayerEvent(
-                                        track = selectedTrack.value,
-                                        type = if (isPlaying) AudioPlayerEventType.PAUSE else AudioPlayerEventType.PLAY
-                                    )
-                                )
-                            }
-                        ) {
-                            Text(
-                                text = if (isPlaying && currentTrackPlaying == trackCollection[track]) "Pause" else "Play"
-                            )
-                        }
-                    }
-                    Column(modifier = Modifier.weight(0.6f)) {
-                        Row {
-                            Text(trackCollection[track].title)
-                        }
-                        Row {
-                            Text(trackCollection[track].artist)
-                        }
-                    }
-                    Column(modifier = Modifier.weight(0.2f)) {
-                        Text(trackCollection[track].duration)
+                    SelectionContainer {
+                        val track = trackCollection.getOrNull(trackIndex) ?: YmeTrack()
+                        TrackRowScreen(
+                            modifier = Modifier.fillMaxWidth(),
+                            trackRow = track,
+                            selectedTrack = selectedTrack,
+                            currentTrackPlaying = currentTrackPlaying,
+                            onMediaPlayerEvent = onMediaPlayerEvent,
+                            isPlaying = isPlaying,
+                        )
+                        Divider()
                     }
                 }
-                Divider()
+            }
+        }
+    }
+}
+
+fun generateContextMenus(): List<ContextMenuItem> {
+    val queueItem = ContextMenuItem("Queue") {
+        println("Queue")
+    }
+    val playlistItem = ContextMenuItem("Playlist") {
+        println("Playlist")
+    }
+    return listOf(queueItem, playlistItem)
+}
+
+@Composable
+fun TrackRowScreen(
+    trackRow: YmeTrack = YmeTrack(),
+    modifier: Modifier = Modifier,
+    selectedTrack: MutableState<YmeTrack>,
+    currentTrackPlaying: YmeTrack,
+    onMediaPlayerEvent: (AudioPlayerEvent) -> Unit,
+    isPlaying: Boolean = false,
+) {
+    Row(
+        modifier = modifier
+            .combinedClickable(
+                onClick = {
+                    selectedTrack.value = trackRow
+                },
+                onDoubleClick = {
+                    selectedTrack.value = trackRow
+                    onMediaPlayerEvent.invoke(
+                        AudioPlayerEvent(
+                            track = selectedTrack.value,
+                            type = if (isPlaying) AudioPlayerEventType.PAUSE else AudioPlayerEventType.PLAY
+                        )
+                    )
+                }
+            )
+    ) {
+        Column(modifier = Modifier.weight(0.2f)) {
+            Button(
+                onClick = {
+                    selectedTrack.value = trackRow
+                    onMediaPlayerEvent.invoke(
+                        AudioPlayerEvent(
+                            track = selectedTrack.value,
+                            type = if (isPlaying) AudioPlayerEventType.PAUSE else AudioPlayerEventType.PLAY
+                        )
+                    )
+                }
+            ) {
+                Text(
+                    text = if (isPlaying && currentTrackPlaying == trackRow) "Pause" else "Play"
+                )
+            }
+        }
+        Column(modifier = Modifier.weight(0.6f)) {
+            if (trackRow.title.isNotEmpty() && trackRow.artist.isNotEmpty()) {
+                Row {
+                    Text(trackRow.title)
+                }
+                Row {
+                    Text(trackRow.artist)
+                }
+            } else {
+                Text("Unknown track")
+            }
+        }
+        Column(modifier = Modifier.weight(0.2f)) {
+            if (trackRow.duration.isNotEmpty()) {
+                Text(trackRow.duration)
+            } else {
+                Text("Unknown duration")
             }
         }
     }
